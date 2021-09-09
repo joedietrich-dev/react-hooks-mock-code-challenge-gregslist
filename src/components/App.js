13e89445e7bb82cf9a1f4ee623bfd2fc76a35777
Joe Dietrich <joe@joedietrich.net>
2021-09-08 21:17:17 -0400

    commit tree d5d940a56a248ad09065a91194dd42c0e0b4cb98

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  const [filterTerm, setFilterTerm] = useState('');
+
+  const searchedListings = listings.filter(listing => listing.description.toLowerCase().includes(filterTerm.toLowerCase()))
+
+  useEffect(() => {
+    fetch('http://localhost:6001/listings')
+      .then(res => res.json())
+      .then(data => setListings(data));
+  }, [])
+
+  const handleDeleteClick = (id) => {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: 'DELETE',
+    }).then(res => {
+      if (res.ok) setListings(listings.filter(listing => listing.id !== id))
+    });
+  }
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setFilterTerm} />
+      <ListingsContainer listings={searchedListings} onDeleteClick={handleDeleteClick} />
     </div>
   );
 }
